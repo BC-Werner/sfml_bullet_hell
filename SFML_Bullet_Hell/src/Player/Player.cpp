2b76aa260acc118602b647671a40130e4e356a3f
Brandon Werner <brandonclayw@gmail.com>
2023-04-24 11:33:02 -0400

    Feat: added player movement and look at mouse functionality

--- SFML_Bullet_Hell/src/Player/Player.cpp
+++ SFML_Bullet_Hell/src/Player/Player.cpp
@@ -1,40 +1,84 @@
 #include "stdafx.h"
 #include "Player.h"
 
-Player::Player(unsigned max_health)
-	: m_health(max_health)
-{
-}
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-Player::Player(unsigned max_health, sf::Texture& texture)
-	: m_health(max_health), m_sprite(texture)
+Player::Player(sf::RenderWindow& window, float size, unsigned max_health)
+	: m_window(window), m_health(max_health), m_cirle_shape(size, 3), m_bounding_circle(size * 0.7f, 16)
 {
+	m_cirle_shape.setOrigin(m_cirle_shape.getRadius(), m_cirle_shape.getRadius());
+	m_bounding_circle.setOrigin(m_bounding_circle.getRadius(), m_bounding_circle.getRadius());
+
+	m_bounding_circle.setFillColor(sf::Color::Transparent);
+	m_cirle_shape.setFillColor(sf::Color::Transparent);
+
+	m_cirle_shape.setOutlineThickness(2.f);
+	m_cirle_shape.setOutlineColor(sf::Color::Red);
+
+	m_bounding_circle.setOutlineThickness(2.f);
+	m_bounding_circle.setOutlineColor(sf::Color::Green);
 }
 
 void Player::handleInput(sf::Event& event)
 {
+	move_flags.up =		sf::Keyboard::isKeyPressed(sf::Keyboard::W);
+	move_flags.down =	sf::Keyboard::isKeyPressed(sf::Keyboard::S);
+	move_flags.left =	sf::Keyboard::isKeyPressed(sf::Keyboard::A);
+	move_flags.right =	sf::Keyboard::isKeyPressed(sf::Keyboard::D);
+
+	// Vertical Movement
+	if (move_flags.up ^ move_flags.down)
+		movement_vector.y = move_flags.up ? -1.f : 1.f;
+	else
+		movement_vector.y = 0.f;
+
+	// Horizontal Movement
+	if (move_flags.left ^ move_flags.right)
+		movement_vector.x = move_flags.left ? -1.f : 1.f;
+	else
+		movement_vector.x = 0.f;
 }
 
-void Player::update()
+void Player::update(float dt)
 {
+	sf::Vector2f dir = movement_vector;
+	float Length = sqrtf(dir.x * dir.x + dir.y * dir.y);
+	sf::Vector2f normalized = dir / (Length == 0.f ? 1.f : Length);
+	set_position(get_position() + (normalized * move_speed * dt));
+
+	// Rotate to look at mouse position
+	sf::Vector2i mouse_pos = sf::Mouse::getPosition(m_window);
+	sf::Vector2f player_pos = get_position();
+	sf::Vector2f D = player_pos - sf::Vector2f(mouse_pos.x, mouse_pos.y);
+
+	float radians = atan2f(D.y, D.x);
+
+	m_cirle_shape.setRotation(radians * 180 / M_PI);
+	m_cirle_shape.rotate(30.f);
 }
 
 void Player::render(sf::RenderWindow& window)
 {
-	window.draw(m_sprite);
+	window.draw(m_cirle_shape);
+	window.draw(m_bounding_circle);
 }
 
 void Player::set_texture(sf::Texture& texture)
 {
-	m_sprite.setTexture(texture);
 }
 
 void Player::set_position(sf::Vector2f position)
 {
-	m_sprite.setPosition(position);
+	m_cirle_shape.setPosition(position);
+	m_bounding_circle.setPosition(position);
+}
+
+const sf::Vector2f Player::get_position() const
+{
+	return m_cirle_shape.getPosition();
 }
 
 void Player::scale(float scalar)
 {
-	m_sprite.scale({ scalar, scalar });
 }
--- SFML_Bullet_Hell/src/Player/Player.h
+++ SFML_Bullet_Hell/src/Player/Player.h
@@ -5,24 +5,37 @@
 class Player : public GameObject
 {
 public:
-	Player() : m_health(0) {}
-	Player(unsigned max_health);
-	Player(unsigned max_health, sf::Texture& texture);
+	Player(sf::RenderWindow& window) : m_window(window), m_health(0) {}
+	Player(sf::RenderWindow& window, float size, unsigned max_health);
 	~Player() {};
 
 	virtual void handleInput(sf::Event& event) override;
-	virtual void update() override;
+	virtual void update(float dt) override;
 	virtual void render(sf::RenderWindow& window) override;
 
 	void set_texture(sf::Texture& texture);
 	void set_position(sf::Vector2f position);
+	const sf::Vector2f get_position() const;
 	void scale(float scalar);
 	 
 private:
+	sf::RenderWindow& m_window;
 	HealthComponent m_health;
 
-	sf::RectangleShape m_shape;
-	sf::Texture m_texture;
-	sf::Sprite m_sprite;
+	sf::CircleShape m_cirle_shape;
+	sf::CircleShape m_bounding_circle;
+
+	sf::Vector2f movement_vector;
+	float move_speed = 100.f;
+
+	struct MoveFlags
+	{
+		bool left = false;
+		bool right = false;
+		bool up = false;
+		bool down = false;
+	};
+
+	MoveFlags move_flags;
 };
 
