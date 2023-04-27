7325ebbc2a4935a724eb59ebfb713e3808f5aefd
Brandon Werner <brandonclayw@gmail.com>
2023-04-27 19:40:46 -0400

    Feat: created basic entity manager

---
+++ SFML_Bullet_Hell/src/Managers/EntityManager.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "EntityManager.h"
+
+EntityManager::EntityManager()
+{
+}
+
+EntityManager::~EntityManager()
+{
+}
---
+++ SFML_Bullet_Hell/src/Managers/EntityManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Objects/GameObject.h"
+
+class EntityManager
+{
+public:
+	EntityManager();
+	~EntityManager();
+
+private:
+	std::map<std::string, GameObject> m_entities;
+};
+
