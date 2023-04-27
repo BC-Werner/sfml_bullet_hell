3a83fedd8d95658b6ec24583e43eef897bd9bc7e
Brandon Werner <brandonclayw@gmail.com>
2023-04-27 16:00:22 -0400

    Fix: separated definition and declarations of random

--- SFML_Bullet_Hell/src/Random.cpp
+++ SFML_Bullet_Hell/src/Random.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
 #include "Random.h"
+
+inline int Random::Int(int lower, int upper) { return Get()._Int(lower, upper); }
+
+inline float Random::Float(float lower, float upper) { return Get()._Float(lower, upper); }
+
+inline int Random::_Int(int lower, int upper)
+{
+	std::uniform_int_distribution<int> dist(lower, upper);
+	return dist(rng);
+}
+
+inline float Random::_Float(float lower, float upper)
+{
+	std::uniform_real_distribution<float> dist(lower, upper);
+	return dist(rng);
+}
--- SFML_Bullet_Hell/src/Random.h
+++ SFML_Bullet_Hell/src/Random.h
@@ -10,25 +10,17 @@ public:
 	{
 		static Random instance;
 		return instance;
-	};
+	}
 
-	static int Int(int lower, int upper) { return Get()._Int(lower, upper); };
-	static float Float(float lower, float upper) { return Get()._Float(lower, upper); };
+	static int Int(int lower, int upper);
+	static float Float(float lower, float upper);
 
 private:
 	Random() { rng = std::mt19937(rd()); };
 
-	int _Int(int lower, int upper)
-	{
-		std::uniform_int_distribution<int> dist(lower, upper);
-		return dist(rng);
-	};
+	int _Int(int lower, int upper);
 
-	float _Float(float lower, float upper)
-	{
-		std::uniform_real_distribution<float> dist(lower, upper);
-		return dist(rng);
-	};
+	float _Float(float lower, float upper);
 
 	std::random_device rd;
 	std::mt19937 rng;
