560d0cf3cedb8d6972570fb4f989dfbdb9cdffa0
Brandon Werner <brandonclayw@gmail.com>
2023-05-02 11:53:48 -0400

    Feat: added shooting component

---
+++ SFML_Bullet_Hell/src/Objects/Components/ShootingComponent.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include "ShootingComponent.h"
+
+ShootingComponent::ShootingComponent()
+{
+}
+
+ShootingComponent::~ShootingComponent()
+{
+}
---
+++ SFML_Bullet_Hell/src/Objects/Components/ShootingComponent.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class ShootingComponent
+{
+public:
+	ShootingComponent();
+	~ShootingComponent();
+
+private:
+};
+
